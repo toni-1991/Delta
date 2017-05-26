1d5f3a3d2d4c705ad9329d6c68474c5e8163f649
toni-1991 <toni.1991@hotmail.com>
2017-05-26 14:21:31 +0200

    Add summary Delta
    Add a first approach of summary Delta, that shown the 3 summary together in 2x2 cases and only the first when it's needed, with proper footer

---
+++ R/summary.Delta.R
@@ -0,0 +1,35 @@
+#' @return \code{NULL}
+#'
+#' @rdname Delta
+#' @export
+#' @method summary Delta 
+
+summary.Delta <- function(x) {
+  temp = x$Summary
+  temp$Summary <- as.character(temp$Summary)
+  if(!is.null(x$Summary_AN)){
+     temp = rbind(temp, 
+            paste(as.character(unlist(x$Summary_AN)[2]),"#") ,
+            paste(as.character(unlist(x$Summary_AE)[2]),"##"))
+    cat('\n',"SUMMARY: Goodness of Fit + Kappa + Delta (Normal)",'\n',
+             "+ Delta (Asintotic Normal) + Delta (Asintotic Extra)",'\n')
+
+              print(temp, row.names = FALSE)
+	# Usar strsplit("test +- lo que sea = 3", "=")[[1]][2] para quedarnos 2a palabra
+	#Separada por = y cambiar delta +- SE y añadir algo de (AN) (AE)
+    cat ("#:  Asintotic normal Delta","\n")
+    cat ("##: Asintotic extra Delta","\n")
+  }
+  else {
+    cat('\n',"SUMMARY: Goodness of Fit + Kappa + Delta",'\n')
+      print(temp, row.names = FALSE)
+  }
+    #Note under some cases;
+	tp = x$tp
+    if (tp == "2.0" | tp == "3.0") {
+      cat("* A total of rows or column is equal to zero: Results obtained adding 0.5 to all cells. ",'\n')
+    }
+    else if (tp == "2.1" | tp == "3.1") {
+      cat("*(Since R(i)=C(i)=x(i,i) for all i, S.E.(kappa) has been obtained adding 0.5 to the original data)",'\n')
+    }
+}
