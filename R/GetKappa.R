3027267f7ad5c683388582225cd4850c1707f572
toni-1991 <toni.1991@hotmail.com>
2017-05-28 12:04:48 +0200

    Update GetKappa.R
    Modify SE calculation

--- R/GetKappa.R
+++ R/GetKappa.R
@@ -28,18 +28,19 @@ GetKappa <-function(mx){
     kappa    = (Observed - Expected)/(n - Expected)
   }
   #Add for aux calculations some matrixs
-  aux_calc	= matrix(rep(R.marg/n,dim.matrix[1]),dim.matrix[1],dim.matrix[1]) + t(matrix(rep(C.marg/n,dim.matrix[1]),dim.matrix[1],dim.matrix[1]))
-  Copy_matrix = mx/n
+  aux_calc	= R.marg/n + C.marg/n
   Observed	= Observed/n
   Expected	= Expected/n
+  Copy_matrix = mx/n
   diag(Copy_matrix) = 0
+  aux_rows =  margin.table(Copy_matrix, 1)
   
   #Calculate SE
   a = sum(diag(mx)/n*(1 - aux_calc * (1-kappa))^2)
-  b = (1-kappa)^2*sum(Copy_matrix * aux_calc^2)
+  b = (1-kappa)^2*sum(aux_rows * aux_calc^2)
   c = (kappa - Expected * (1-kappa))^2
-  SE = 1/(1-Expected)^2*sqrt((a+b-c)/n)
-  res = list("kappa"=kappa,"SE"=SE)
+  SE = 1/(1-Expected) * sqrt((a+b-c)/n)
+  res = list("kappa" = kappa, "SE" = SE)
   class(res) <- "GetKappa"
   return(res)
 }
